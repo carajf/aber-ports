1286ff22b69fb57806fa765011372f5fa9bb7933
Cara <47179416+caramelfish@users.noreply.github.com>
2021-04-08 17:36:49 +0100

    Update transfer_all_ship_data.py
    Python file which transfers all of the ship data from the excel spreadsheets to json dictionaries in Python using the get_ships.py script.

--- transfer_all_ship_data.py
+++ transfer_all_ship_data.py
@@ -1,31 +1,33 @@
-# transfer ship data to a MongoDB collection
+### PYTHON SCRIPT TO TRANSFER SHIP DATA TO A DOCUMENT DATABASE.
 
-# get a connection to MongoDB
-from pymongo
+# Establish connection to mongodb.
+import pymongo
+from pymongo import MongoClient
 
-user = 'eds'
-dbpath = 'nosql.dcs.aber.ac.uk/eds'
-password = '*********'
-connection_string = 'mongodb://'+user+':'+password+'@'+dbpath
+client = MongoClient('mongodb://localhost:27017')
+db = client.aber_ship_db
 
-client = pymongo.MongoClient(connection_string)
-
-db = client.eds
-
-# walk the ship data directory to find the excel files
-
+# Import necessary modules (including get_ships script which retrieves the data from the xlxs files and converts to JSON)
 import os
 import get_ships
 
+# Establish the list in which ships will be stored
 all_ships = []
 
-ships_dir = '/aber/eds/mongodb/prac2/ABERSHIP_transcription_vtls004566921/ABERSHIP_transcription_vtls004566921'
+# Locating the root directory of all ship data
+ships_dir = 'Data\ABERSHIP_transcription_vtls004566921'
 
+# Walking through each file in the directory
 for root, dirs, files in os.walk(ships_dir):
     for file in files:
         name, ext = os.path.splitext(file)
         if ext == '.xlsx':
-            all_ships += get_ships.get_ships( os.path.join(root, file) )
 
-result = db.eds.insert_many(all_ships)
+            # Adding the data in the csv->json file to the all_ships list
+            all_ships += get_ships.get_ships( os.path.join(root, file))
+
+# Inserting the data in the all_ships list into the database
+result = db.aber_ships.insert_many(all_ships)
+
+# Printing the results of the insert command into the console
 print(result)
