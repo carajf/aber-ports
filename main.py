25f90539245b5dbce44d6118d8299e446f904c58
Cara <47179416+caramelfish@users.noreply.github.com>
2021-04-08 17:35:32 +0100

    Update main.py
    --Updated with test queries.
    --Updated mongo connection URL.

--- main.py
+++ main.py
@@ -2,8 +2,29 @@
 import pymongo
 from pymongo import MongoClient
 
-client = MongoClient('mongodb://caf39:NaanNNHVEzzv@nosql.dcs.aber.ac.uk/caf39')
-db = client.caf39
+client = MongoClient('mongodb://localhost:27017/?readPreference=primary&appname=MongoDB%20Compass&ssl=false')
+db = client.adm_db
+
+## INSERT TEST DATA
+def insert_test_data():
+  result = db.ships.insert_one({
+     "ship": "Black Pearl"
+  })
+
+# insert_test_data()
+
+## TEST QUERY
+def test_query():
+  result = db.ships.find( {"$or":
+                          [ 
+                            {"ship": {"$exists": True}},
+                            {"flag": {"$exists": True}}
+                          ] } )
+
+  for doc in result:
+    print(doc)
+
+# test_query()
 
 ## TRANSFORM CSV TO JSON
 
