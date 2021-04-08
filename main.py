7bc15729c6b1a68f1e7716d2ca411ffb0759909d
Cara <47179416+caramelfish@users.noreply.github.com>
2021-04-08 13:09:40 +0100

    Created main.py

---
+++ main.py
@@ -0,0 +1,11 @@
+# SETTING UP THE DATABASE
+import pymongo
+from pymongo import MongoClient
+
+client = MongoClient('mongodb://caf39:NaanNNHVEzzv@nosql.dcs.aber.ac.uk/caf39')
+db = client.caf39
+
+## TRANSFORM CSV TO JSON
+
+
+
